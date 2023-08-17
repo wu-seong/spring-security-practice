ab3c7112edfb7bf4cc4ffe60cb0bd2225585cf3f
wuseong <jws001228@naver.com>
2023-08-17 15:56:55 +0900

    feat: 임시 유저추가, filterChain 설정
    Role별로 메모리 유저 추가함. 인가, 폼로그인, 인증/인가 예외처리 설정.

--- src/main/java/io/security/springsecuritypractice/security/SecurityConfiguration.java
+++ src/main/java/io/security/springsecuritypractice/security/SecurityConfiguration.java
@@ -1,39 +1,107 @@
 package io.security.springsecuritypractice.security;
 
 
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AnonymousConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.access.expression.DefaultHttpSecurityExpressionHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.CookieClearingLogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import java.io.IOException;
+
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
-    private final UserDetailsService userDetailService;
+    @Bean
+    public UserDetailsManager users() {
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{noop}1111")
+                .roles("USER")
+                .build();
+
+        UserDetails sys = User.builder()
+                .username("sys")
+                .password("{noop}1111")
+                .roles("SYS")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}1111")
+                .roles("ADMIN", "SYS", "USER")
+                .build();
+
+        return new InMemoryUserDetailsManager( user, sys, admin );
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
+        http.
+                authorizeHttpRequests(auth ->{
+                    auth.
+                            requestMatchers("/login").permitAll().
+                            requestMatchers("/user").hasRole("USER").
+                            requestMatchers("/admin/pay").hasRole("ADMIN").
+                            requestMatchers("/admin/**").hasAnyRole("ADMIN","SYS").
+                            anyRequest().authenticated();
+                });
+        http.
+                formLogin(login ->{
+                    login.successHandler(new AuthenticationSuccessHandler() {
+                        @Override
+                        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                            RequestCache requestCache = new HttpSessionRequestCache(); // requestcache를 가져옴
+                            SavedRequest savedRequest = requestCache.getRequest(request, response); //요청헤더에 있는 세션id를 통해 saved된 요청을 가져옴
+                            String redirectUrl = savedRequest.getRedirectUrl();
+                            response.sendRedirect(redirectUrl);
+                        }
+                    });
+                });
 
+
+
+        http.
+                exceptionHandling( httpSecurityExceptionHandlingConfigurer -> {
+                    httpSecurityExceptionHandlingConfigurer
+//                            .authenticationEntryPoint(new AuthenticationEntryPoint() { //인증 예외
+//                                @Override
+//                                public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+//                                    response.sendRedirect("/login");
+//                                }
+//                            })
+                            .accessDeniedHandler(new AccessDeniedHandler() { //인가 예외
+                                @Override
+                                public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                                    response.sendRedirect("/denied");
+                                }
+                            });
+                });
         return http.build();
     }
 }
